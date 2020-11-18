bd297dc4daf883ef58ceae44d068197ffb3e048f
Justin Yoo <justin.yoojh@gmail.com>
2020-11-18 19:43:58 +0900

    Update function app location

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -1,5 +1,5 @@
 export default {
-  apiBaseUrl: process.env.VUE_APP_API_BASE_URL || '',
+  apiBaseUrl: process.env.VUE_APP_API_BASE_URL || 'https://fncapp-caption-krc.azurewebsites.net',
   region: process.env.VUE_APP_COGNITIVE_API_REGION || 'koreacentral',
   title: process.env.VUE_APP_TITLE || 'CaptionR'
 }
